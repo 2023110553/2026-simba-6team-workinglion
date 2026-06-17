faa3b923084cfbe0d8eda3363edf382ce0cd2beb
dPwls52 <limyj05201@gmail.com>
2026-06-17 16:42:57 +0900

    dashboard, create_pot, join_pot url연결

--- main/urls.py
+++ main/urls.py
@@ -7,4 +7,7 @@ app_name = 'main'
 urlpatterns = [
     path('onboarding/', views.onboarding, name='onboarding'),
     path('signup_login/', views.signup_login, name='signup_login'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('create_pot/', views.create_pot, name='create_pot'),
+    path('join_pot/', views.join_pot, name='join_pot'),
 ]
